43abf228f832e275b2f33d5bad271f213c40a566
xuyang.cao <xuyang.cao@garena.com>
2025-05-06 19:48:16 +0800

    client use the same agent

--- unfazed_taskiq/cli.py
+++ unfazed_taskiq/cli.py
@@ -1,40 +1,5 @@
-import os
+from .base import agent
 
-from unfazed.utils import import_setting
+broker = agent.broker
 
-from .base import TaskiqAgent
-from .settings import UnfazedTaskiqSettings
-
-
-def get_agent() -> TaskiqAgent:
-    """
-    interface for getting taskiq client
-
-    Example:
-        taskiq worker unfazed_taskiq.cli:broker
-        taskiq worker unfazed_taskiq.cli:scheduler
-    """
-
-    if not os.environ.get("UNFAZED_SETTINGS_MODULE"):
-        raise ValueError("UNFAZED_SETTINGS_MODULE is not set")
-
-    # extract settings from unfazed settings
-    settings_kv = import_setting("UNFAZED_SETTINGS_MODULE")
-
-    # extract broker settings
-    taskiq_settings = settings_kv["UNFAZED_TASKIQ_SETTINGS"]
-
-    settings = UnfazedTaskiqSettings.model_validate(taskiq_settings)
-
-    agent = TaskiqAgent()
-    agent.setup(settings)
-
-    return agent
-
-
-_agent: TaskiqAgent = get_agent()
-
-
-broker = _agent.broker
-
-scheduler = _agent.scheduler
+scheduler = agent.scheduler
