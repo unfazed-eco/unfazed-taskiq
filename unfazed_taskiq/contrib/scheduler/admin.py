9ba6fbe1a64400806a74b36001ab66b1f907cd94
yanwei.zhu <yanwei.zhu@garena.cn>
2025-09-21 12:17:59 +0800

    feat/support-mysql-scheduler-nuli-broker

--- unfazed_taskiq/contrib/scheduler/admin.py
+++ unfazed_taskiq/contrib/scheduler/admin.py
@@ -3,6 +3,7 @@ from unfazed.contrib.admin.registry import ModelAdmin, register
 from . import serializer as s
 
 
-@register(serializer_cls=s.PeriodicTaskSerializer)
+@register(s.PeriodicTaskSerializer)
 class PeriodicTaskAdmin(ModelAdmin):
-    list_display = ("task_name", "description", "cron", "time", "enabled")
+    route_label = "TaskIQ"
+    component="ModelAdmin"
